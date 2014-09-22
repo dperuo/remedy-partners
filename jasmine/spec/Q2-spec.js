c48ba02ba7ca5171d6ae9d52e9f4501775315aa5
dperuo <derek@derekperuo.net>
2014-09-22 17:11:10 -0400

    start scripting

---
+++ jasmine/spec/Q2-spec.js
@@ -0,0 +1,13 @@
+(function() {
+"use strict";
+
+describe('Q2: Important Fruit', function() {
+
+  it('returns true if fruit is important', function() {
+    expect(isImportantFruit).toEqual(true)
+  })
+})
+
+
+
+})(); // IIFE
