1e45fd99ce1b73638b81195277a92bfbb97fddb6
dperuo <derek@derekperuo.net>
2014-09-22 17:14:05 -0400

    prelims

--- Q1.js
+++ Q1.js
@@ -1,3 +1,6 @@
+// 1. Use “each” lambda-style function instead of “for” statement:
+
+
 var sum = 0;
 var arr = [ 41, 12, 32, 344, 15 ];
 for ( var i = 0, l = arr.length; i < l; i++ )
--- Q2.js
+++ Q2.js
@@ -1,10 +1,15 @@
+// 2. Make code more flexible for changes considering that the list of important fruits
+// will grow, add 5 more fruits as example.
+
+
+
 (function () {
 "use strict";
 
   if (fruit == 'apple' || fruit == 'orange' || fruit == 'lemon') {
-            isImportantFruit = true;
+    isImportantFruit = true;
   } else {
-            isImportantFruit = false;
+    isImportantFruit = false;
   }
 
 })(); // IIFE
