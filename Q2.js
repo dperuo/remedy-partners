34f5fe031ddad09a2ed8e549e5938592ea9c0015
dperuo <derek@derekperuo.net>
2014-09-23 22:45:42 -0400

    declare fruit var

--- Q2.js
+++ Q2.js
@@ -1,6 +1,8 @@
 // 2. Make code more flexible for changes considering that the list of important fruits
 // will grow, add 5 more fruits as example.
 
+var fruit;
+
 if (fruit == 'apple' || fruit == 'orange' || fruit == 'lemon') {
   isImportantFruit = true;
 } else {
