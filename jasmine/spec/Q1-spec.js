df8d6020dbc5286462feee8d6e5af8742af0533e
dperuo <derek@derekperuo.net>
2014-09-23 21:22:54 -0400

    spec for Q1

---
+++ jasmine/spec/Q1-spec.js
@@ -0,0 +1,9 @@
+(function() {
+"use strict";
+
+  describe("Q1: Lambda Function", function (){
+    it('returns the sum 444', function (){
+      expect(sum).toEqual(444)
+    })
+  })
+})(); // IIFE
