f088ac72690a7fef525f71c0071001d6b5df7cf5
dperuo <derek@derekperuo.net>
2014-09-23 22:15:26 -0400

    reset

--- Q2.js
+++ Q2.js
@@ -1,15 +1,8 @@
 // 2. Make code more flexible for changes considering that the list of important fruits
 // will grow, add 5 more fruits as example.
 
-
-
-(function () {
-"use strict";
-
-  if (fruit == 'apple' || fruit == 'orange' || fruit == 'lemon') {
-    isImportantFruit = true;
-  } else {
-    isImportantFruit = false;
-  }
-
-})(); // IIFE
+if (fruit == 'apple' || fruit == 'orange' || fruit == 'lemon') {
+  isImportantFruit = true;
+} else {
+  isImportantFruit = false;
+}
--- jasmine/spec/Q2-spec.js
+++ jasmine/spec/Q2-spec.js
@@ -3,8 +3,5 @@
 
 describe('Q2: Important Fruit', function() {
 
-  xit('returns true if fruit is important', function() {
-    expect(isImportantFruit).toEqual(true)
-  })
 })
 })(); // IIFE
