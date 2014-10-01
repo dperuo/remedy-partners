027643d7f62e55da01d76db6292279f5f7e0bbb8
dperuo <derek@derekperuo.net>
2014-10-01 12:03:07 -0400

    deleteBookFn()

--- Q3/app/js/app.js
+++ Q3/app/js/app.js
@@ -10,29 +10,37 @@ function ctrl(localStash) {
   vm.newBook       = {};
   vm.bookList      = [];
   vm.confirmDelete = false;
+  vm.saveNewBook   = saveNewBookFn;
+  vm.deleteBook    = deleteBookFn;
 
-  vm.saveNewBook   = function(newBook) {
+
+
+
+
+  function saveNewBookFn(newBook) {
     newBook.id = vm.bookList.length + 1;
     localStash.set(newBook.id, newBook);
     vm.bookList.push(localStash.get(newBook.id));
   };
 
+  function deleteBookFn(index) {
+    vm.bookList.splice(index, 1);
+  }
 
+  // vm.deleteBook = function() {alert("I'm deleted!");};
 
-  vm.deleteBook = function() {alert("I'm deleted!");};
+  // (function populateBookList () {
 
-  (function populateBookList () {
-
-    if (localStash.length > 0) {
-      for (var i = 0, l = vm.bookList.length; i < l; i++) {
-        vm.bookList.push(localStash.get(localStash.key(i)))
-      }
-    } else {
-      vm.bookList.push(Book({id: vm.bookList.length + 1, title: "Dracula",       author: "Bram Stoker",         price: 1.99}));
-      vm.bookList.push(Book({id: vm.bookList.length + 1, title: "Walden",        author: "Henry David Thoreau", price: 2.99}));
-      vm.bookList.push(Book({id: vm.bookList.length + 1, title: "Invisible Man", author: "Ralph Ellison",       price: 3.99}));
-    }
-  })()
+  //   if (localStash.length > 0) {
+  //     for (var i = 0, l = vm.bookList.length; i < l; i++) {
+  //       vm.bookList.push(localStash.get(localStash.key(i)))
+  //     }
+  //   } else {
+  //     vm.bookList.push(Book({id: vm.bookList.length + 1, title: "Dracula",       author: "Bram Stoker",         price: 1.99}));
+  //     vm.bookList.push(Book({id: vm.bookList.length + 1, title: "Walden",        author: "Henry David Thoreau", price: 2.99}));
+  //     vm.bookList.push(Book({id: vm.bookList.length + 1, title: "Invisible Man", author: "Ralph Ellison",       price: 3.99}));
+  //   }
+  // })()
 
 }
 
